f17e216e062ac96415ec6aec42e4f19e98657c65
Adrian Corral <Adrian Corral@DESKTOP-R7PF1G3>
2021-04-21 12:58:08 -0600

    Primer ejercicio, mostrar saludos

--- src/ejercicios_github/Ejercicios_GitHub.java
+++ src/ejercicios_github/Ejercicios_GitHub.java
@@ -1,21 +1,19 @@
 ///https://github.com/AdriCoQue/Ejercicios_POO.git
 package ejercicios_github;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Ejercicios_GitHub {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String nombre, inicial="";
-        System.out.println("Ingresa tu nombre completo");
-        nombre =sc.nextLine();
-        System.out.println("Hola " + nombre);
+        nombre = JOptionPane.showInputDialog("Ingresa tu nombre completo");;
+        JOptionPane.showMessageDialog(null, "Hola " + nombre, "Hola", JOptionPane.INFORMATION_MESSAGE);
         String []  iniciales = nombre.split(" ");
         for (String iniciale : iniciales) {
             inicial += iniciale.charAt(0);
         }
-        System.out.println("Sus iniciales son: " + inicial.toUpperCase());
+        JOptionPane.showMessageDialog(null, "Sus iniciales son: " + inicial.toUpperCase(), "Iniciales", JOptionPane.INFORMATION_MESSAGE);
     }
     ///Agregar cambios a ver si le sé
 }
