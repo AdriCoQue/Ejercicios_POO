814bdb840a4d01d5cbb4640375c2e6035e2c8ee5
Adrian Corral <Adrian Corral@DESKTOP-R7PF1G3>
2021-04-22 13:00:32 -0600

    Retorno de valores con formato de salida

--- src/ejercicios_github/Formatos.java
+++ src/ejercicios_github/Formatos.java
@@ -18,9 +18,9 @@ public class Formatos {
         String cabecera = "\n\tPRÓNOSTICO DEL CLIMA:\n";
         cabecera += "\n\tDía\t\tMañana\tNoche\tCondiciones\n";
         cabecera += "\t---\t\t---\t---\t---\n";
-        String pronostico = "\tDomingo\t25°C\t\t33°C\t\tSoleado\n";
-        pronostico += "\tLunes\t18°C\t\t19°C\t\tNublado\n";
-        pronostico += "\tMartes\t40°C\t\t29°C\t\tCaluroso\n";
+        String pronostico = "\tDomingo\t\t25°C\t33°C\tSoleado\n";
+        pronostico += "\tLunes\t\t18°C\t19°C\tNublado\n";
+        pronostico += "\tMartes\t\t40°C\t29°C\tCaluroso\n";
         System.out.print(cabecera + pronostico);
     }
     
