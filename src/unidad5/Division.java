15c03361a48c84e26bf7e9139216b75385ade3ba
Adrian Corral <Adrian Corral@DESKTOP-R7PF1G3>
2021-06-18 12:33:32 -0600

    Ejemplos poo

--- src/unidad5/Division.java
+++ src/unidad5/Division.java
@@ -4,6 +4,8 @@ package unidad5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 /**
  *
@@ -16,8 +18,8 @@ public class Division {
         String str1 = "100";
         String str2 = "0";
         String respuesta = "";
-        int num;
-        int den;
+        double num;
+        double den;
         double cociente;
         InputStreamReader s = new InputStreamReader(System.in);
         BufferedReader stdin = new BufferedReader(s);
@@ -30,7 +32,10 @@ public class Division {
             System.out.println("Ingrese el denominador");
             den = Integer.parseInt(stdin.readLine());
             cociente = calcular(str1, str2, num, den);
-            respuesta = String.valueOf(cociente);
+            DecimalFormatSymbols separador = new DecimalFormatSymbols();
+            separador.setDecimalSeparator('.');
+            DecimalFormat formato = new DecimalFormat("#.####", separador);
+            respuesta = String.valueOf(formato.format(cociente));
         }catch(NumberFormatException nex){
             respuesta = "Se ha introducido un caracter no numérico";
         }catch(ArithmeticException aex){
@@ -44,14 +49,18 @@ public class Division {
         System.out.println("Resultado: " + respuesta);
         
     }
-    public static double calcular(String str1, String str2, int num, int den) throws IntervaloException, NumberFormatException, ArithmeticException{
+    public static double calcular(String str1, String str2, double num, double den) throws IntervaloException, NumberFormatException, ArithmeticException{
         int limSup = Integer.parseInt(str1);
         int limInf = Integer.parseInt(str2);
+        if(den>0){
         if(num<=limSup && num>=limInf && den<=limSup && den>=limInf){
         double resultado = (num/den);
         return resultado;    
         }else{
             throw new IntervaloException("Ingresó un valor fuera del límite permitido");
+        }
+        } else{
+            throw new ArithmeticException();
         }
     }
     
